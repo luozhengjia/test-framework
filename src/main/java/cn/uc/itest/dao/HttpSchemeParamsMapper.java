80fb9fd05b1683b6e441b58b26f3f6d45d46c3ec
罗正加 <luozj@ucweb.com>
2013-09-01 10:03:06 +0800

    定义mapper

--- src/main/java/cn/uc/itest/dao/HttpSchemeParamsMapper.java
+++ src/main/java/cn/uc/itest/dao/HttpSchemeParamsMapper.java
@@ -2,12 +2,12 @@ package cn.uc.itest.dao;
 
 
 /**
- * 应用服务接口
+ * http参数 服务接口
  * 
  * @author 罗正加
  * @history 2013-08-25 罗正加 新建
  */
-public interface ApplicationMapper {
+public interface HttpSchemeParamsMapper {
 
 
 }
